98a7aed3835c83199a284a297bf4385a8f39cb46
R1jul <rijul01b@gmail.com>
2025-10-24 00:44:32 +0530

    Done in PEP8 format with Chagpt's help

--- cli_tools/main.py
+++ cli_tools/main.py
@@ -4,18 +4,12 @@ def greet(name :str) :
     return f"Hey wassup,{name}"
 
 
-
-
-
 def main():
     parser = argparse.ArgumentParser(description="For my mini project")
     parser.add_argument("name")
     args = parser.parse_args()
     print(greet(args.name))
-  
-  
-  
-  
     
+        
 if __name__ == "__main__":
     main()
