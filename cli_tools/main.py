72aecdbe66aa044f0dbc192a42d3979e34592c0b
R1jul <rijul01b@gmail.com>
2025-10-24 00:48:24 +0530

    is flake8 for real

--- cli_tools/main.py
+++ cli_tools/main.py
@@ -1,15 +1,16 @@
 import argparse
 
-def greet(name :str) :
+
+def greet(name : str) :
     return f"Hey wassup,{name}"
 
 
-def main():
+def main() :
     parser = argparse.ArgumentParser(description="For my mini project")
     parser.add_argument("name")
     args = parser.parse_args()
     print(greet(args.name))
-    
-        
-if __name__ == "__main__":
+
+
+if __name__ == "__main__" :
     main()
